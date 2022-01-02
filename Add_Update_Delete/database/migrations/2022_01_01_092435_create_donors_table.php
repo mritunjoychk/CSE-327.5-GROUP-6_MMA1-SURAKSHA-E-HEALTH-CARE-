2a2d974b96396ef2e63a44f42e92a7e07f947176
KomolNath <80051933+KomolNath@users.noreply.github.com>
2022-01-02 15:22:37 +0600

    Add files via upload

---
+++ Add_Update_Delete/database/migrations/2022_01_01_092435_create_donors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDonorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('age')->nullable();
+            $table->string('blood_group')->nullable();
+            $table->string('contact')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donors');
+    }
+}
